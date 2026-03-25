ccde57d3cd5d0eeae8b8c54b00cc658368cb5d02
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-03-25 11:51:40 -0300

    adicionei aprendizado

--- Teste01.py
+++ Teste01.py
@@ -1,2 +1,10 @@
 print('Ola, mundo!')
-input('Diga seu nome')
+nome = str(input('Diga seu nome: '))
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1 + n2
+#print('A soma entre', n1, 'e', n2, 'vale', s)
+#print(type(nome))
+#print(type(n1))
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+print(nome.isnumeric())
