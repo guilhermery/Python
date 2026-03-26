9ddaccbf86ca1406c2d75df7830e857a88f356ee
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-03-26 13:23:33 -0300

    Adicionei exercícios 011 e 012

---
+++ Exercícios/Exercício011.py
@@ -0,0 +1,3 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+num = int(input('Informe um número: '))
+print('O dobro de {} é {}. O triplo do mesmo número é {} e a raiz quadrada dele é {:.0f}.'.format(num, num*2, num*3, num**0.5))
---
+++ Exercícios/Exercício012.py
@@ -0,0 +1,6 @@
+#Faça um programa que quantos reais alguém possui e quantos dólares ela pode comprar.
+n = float(input('Informe quantos reais você possui: R$'))
+print('Você pode comprar ${:.02f}'.format(n/3.27))
+
+
+
