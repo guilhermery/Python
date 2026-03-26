bffea04b1ab21655cb97bcb9375b55248a40ba81
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-03-26 10:30:37 -0300

    Adicionei os exercícios 003, 004 e 005

---
+++ Exercícios/Exercício004.py
@@ -0,0 +1,6 @@
+num = float(input('Informe um número: '))
+if num % 2 == 0:
+    print('O número {} é par'.format(num))
+else:
+    print('O número {} é ímpar'.format(num))
+    
---
+++ Exercícios/Exercício005.py
@@ -0,0 +1,3 @@
+c = float(input('Informe a temperatura em Celsius: '))
+f = c * 1.8 + 32
+print('A temperatura em graus Farenheit é de {}'.format(f))
