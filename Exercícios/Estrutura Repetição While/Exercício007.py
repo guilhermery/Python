b62b4e63daab32ada9954856e8f411ecbd5b8bfd
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-05-02 12:54:32 -0300

    Estrutura While: Realizei uma pequena alteração no exercicio 007 com while true.

--- Exercícios/Estrutura Repetição While/Exercício007.py
+++ Exercícios/Estrutura Repetição While/Exercício007.py
@@ -1,6 +1,9 @@
-soma = n = 0
-while n != 999:
+soma = n = cont = 0
+while True:
     n = int(input('Informe um número: '))
     if n != 999:
         soma += n
-print(f'A soma entre os números informados é igual a: {soma}')
+        cont += 1
+    else:
+        break
+print(f'A soma entre os {cont} números informados é igual a: {soma}')
