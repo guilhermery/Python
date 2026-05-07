6c9bda93493f854a6a968c57ccc9b22d639d7ad6
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-05-07 14:23:58 -0300

    feat(Listas): Exercício011 aprimorado com mais funcionalidades

--- Exercícios/Listas/Exercício011.py
+++ Exercícios/Listas/Exercício011.py
@@ -1,4 +1,5 @@
 matriz = [[],[],[]]
+somap = soma3 = maior = 0
 for i in range(3):
     for c in range(3):
         matriz[i].append(int(input(f'Digite um valor para [{i}, {c}]: ')))
@@ -6,5 +7,18 @@ print('-='*30)
 for i in range(3):
     for c in range(3):
         print(f'[  {matriz[i][c]}  ]', end=' ')
+        if matriz[i][c] % 2 == 0:
+            somap += matriz[i][c]
+        if c == 2:
+            soma3 += matriz[i][c]
+        if i == 1:
+            if c == 0:
+                maior = matriz[i][c]
+            elif matriz[i][c] > maior:
+                maior = matriz[i][c]
     print('')
+print('-='*30)
+print(f'A soma dos valores pares é {somap}.')
+print(f'A soma dos valores da terceira coluna é {soma3}.')
+print(f'O maior valor da segunda linha é {maior}.')
 
