f196614510bbfa3dc631130fc2dfc846dbe42587
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-05-17 18:25:36 -0300

    feat(Funções): Adicionei parametros opcionais no exercício007

--- Exercícios/Funções/Exercício007.py
+++ Exercícios/Funções/Exercício007.py
@@ -1,5 +1,6 @@
 #docstrings são strings de documentação como as que são mostradas acima
-def contador(i, f, p):
+#colocar parametros opcionais são importantes para o caso de não serem colocados todos os valores na chamada
+def contador(i=0, f=0, p=1):
     """
     -> Faz uma contagem e mostra na tela.
     :param i: início da contagem
@@ -13,4 +14,17 @@ def contador(i, f, p):
         c += p
     print('FIM!')
 
+def somar(a=0, b=0, c=0):
+    """
+    -> Faz a soma de três valores e mostra o resultado.
+    :param a: primeiro valor
+    :param b: segundo valor
+    :param c: terceiro valor
+    :return: sem retorno
+    """
+    s = a + b + c
+    print(f'A soma vale {s}')
+
 help(contador)
+somar(3, 4, 6)
+somar(4, 3)
