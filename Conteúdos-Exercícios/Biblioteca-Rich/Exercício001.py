dc244402adf6a2af449be4d93f679431780cbfaa
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-06-10 09:21:25 -0300

    feat(Biblioteca-Rich): adiciona exercícios 001 e 002 sobre paineis e cores

---
+++ Conteúdos-Exercícios/Biblioteca-Rich/Exercício001.py
@@ -0,0 +1,6 @@
+from rich import print
+
+#Para ver todos os emojis da biblioteca. python -m rich.emoji no terminal.
+print('Olá, [red]Mundo[/]! :earth_americas:')
+print('Olá, [bold blue on white]Guilherme[/]')
+print(':+1: :high_brightness: :-1:')
---
+++ Conteúdos-Exercícios/Biblioteca-Rich/Exercício002.py
@@ -0,0 +1,6 @@
+from rich import print
+from rich.panel import Panel
+
+caixa = Panel("[white]Esse é um painel de exemplo[/]", title='Mensagem', style='Red', width=35)
+
+print(caixa)
