65103ae37d21067edd8bcc10c8d64c6e7447d96a
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-06-13 23:02:21 -0300

    feat(POO_Classes): Adiciona exercício008

---
+++ Conteúdos_Exercícios/POO_Herança_Polimorfismo/Exercício008/Exercício008.py
@@ -0,0 +1,11 @@
+from classes import Avaliacao
+from rich import print, inspect
+
+def main():
+    av1 = Avaliacao("Guilherme", "Sistemas Computacionais", 9.5)
+    av1.nota = 10
+    print(f'{av1.nome} tirou {av1.nota} em {av1.disciplina}')
+    inspect(av1, private=True)
+
+if __name__ == "__main__":
+    main()
