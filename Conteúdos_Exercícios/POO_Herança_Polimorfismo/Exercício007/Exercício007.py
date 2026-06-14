37602b57caffb1e93da1a4d8dd9f7b7bb0e42223
Guilherme Ryan <guilhermeryan277@gmail.com>
2026-06-13 23:01:43 -0300

    refactor(POO_Classes): altera exercício007

--- Conteúdos_Exercícios/POO_Herança_Polimorfismo/Exercício007/Exercício007.py
+++ Conteúdos_Exercícios/POO_Herança_Polimorfismo/Exercício007/Exercício007.py
@@ -3,7 +3,8 @@ from rich import print, inspect
 
 def main():
     av1 = Avaliacao("Guilherme", "Sistemas Computacionais", 9.5)
-    av1.nota = -234
+    av1.set_nota(10)
+    print(f'{av1.nome} tirou {av1.get_nota()} em {av1.disciplina}')
     inspect(av1, private=True)
 
 if __name__ == "__main__":
--- Conteúdos_Exercícios/POO_Herança_Polimorfismo/Exercício007/classes.py
+++ Conteúdos_Exercícios/POO_Herança_Polimorfismo/Exercício007/classes.py
@@ -1,8 +1,16 @@
 class Avaliacao:
-    def __init__(self, nome, disciplina, nota):
+    def __init__(self, nome, disciplina, nota = 0):
         self.nome = nome
         self.disciplina = disciplina
         self._nota = nota
 
-    
+    # Metodos acessores:
 
+    def get_nota(self): # Metodo Getter
+        return self._nota
+
+    def set_nota(self, valor): # Metodo Setter
+        if 0 <= valor <= 10:    
+            self._nota = valor
+        else:
+            print("Nota inválida!")
